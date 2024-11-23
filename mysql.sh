a192312f8217a0f4e2ef016526e265b923d9316e
kisskasa <chandrasekharkasa@gmail.com>
2024-11-23 15:51:19 +0530

    sg

--- mysql.sh
+++ mysql.sh
@@ -13,13 +13,11 @@ systemctl enable mysqld &>>$LOGFILE
 
 systemctl start mysqld &>>$LOGFILE
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-
 # Below code will be useful for idempotent nature
 mysql -h db.chandrakasa.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation123 --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
