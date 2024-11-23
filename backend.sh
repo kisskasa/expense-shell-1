abe068b7cdaea78fd068e8c239a71a23af3bf888
kisskasa <chandrasekharkasa@gmail.com>
2024-11-23 16:03:18 +0530

    sg

--- backend.sh
+++ backend.sh
@@ -5,7 +5,7 @@ source ./common.sh
 check_root
 
 echo "Please enter DB password:"
-read -s mysql_root_password
+read mysql_root_password
 
 dnf module disable nodejs -y &>>LOGFILE
 dnf module enable nodejs:20 -y &>>LOGFILE
@@ -29,7 +29,7 @@ unzip /tmp/backend.zip &>>LOGFILE
 
 npm install &>>LOGFILE
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>LOGFILE
 
 systemctl daemon-reload &>>LOGFILE
 
